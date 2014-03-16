582e940cfcd218d5ddbbb4fdc8ab64a6369e73a0
James W. Lane <jlane@cmn.com>
2014-03-15 22:46:49 -0500

    Finished Render Class

--- render-schema-data.php
+++ render-schema-data.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * TablesPress Schema Data View class
+ * TablesPress Render Schema Data class
  *
  * @since 1.0.0
  */
@@ -11,46 +11,53 @@ defined( 'ABSPATH' ) || die( 'No direct script access allowed!' );
 class TablePress_Render_Schema_Data extends TablePress_Render {
 
 	/**
-	 * Initialize the Rendering class, include the EvalMath class
+	 * We hook into the tag_custom_attr and tr_custom_attr to add the Schema Data
 	 *
 	 * @since 1.0.0
 	 */
 	public function __construct() {
-		add_filter( 'tablepress_table_render_data', array( __CLASS__, 'tablepress_render_data' ), 10, 3 );
-		add_filter('tbody_custom_attr', array( __CLASS__, 'tag_custom_attr' ), 10, 7  );
 		add_filter('tag_custom_attr', array( __CLASS__, 'tag_custom_attr' ), 10, 7  );
 		add_filter('tr_custom_attr', array( __CLASS__, 'tr_custom_attr' ), 10, 4  );
 	}
 
-	function tablepress_render_data( $table, $orig_table, $render_options ) {
-		return $table;
-	}
-
-    public function tbody_custom_attr( $data, $table_id, $cell_content, $row_idx, $col_idx, $colspan_row_idx, $rowspan_col_idx ) {
-		$id = $table_id['id'];
-		$value = '$id="' . $id . '"$col_idx="' . $col_idx . '" $colspan_row_idx="' . $colspan_row_idx . '"';
-		return $value;
-	}
-
+	// <td> Schema Data is generated here
     public function tag_custom_attr( $data, $table_id, $cell_content, $row_idx, $col_idx, $colspan_row_idx, $rowspan_col_idx ) {
 		$id = $table_id['id'];
-		$value = '$id="' . $id . '"$col_idx="' . $col_idx . '" $colspan_row_idx="' . $colspan_row_idx . '"';
+		$value = '';
+
+		// Check to see if we are in a header column
+		if ( 1 !== $col_idx ){
+			global $wpdb;
+			$schema_data = $wpdb->get_var("SELECT schema_data FROM {$wpdb->prefix}tablepress_schema_data WHERE table_id = $id");
+			$schema_data = unserialize( $schema_data );
+
+			// Check to make sure a itemprop value has been set
+			if ( !empty( $schema_data[$colspan_row_idx] ) ){
+				$schema_data_itemprop = $schema_data[$colspan_row_idx];
+				$value = 'itemprop="' . $schema_data_itemprop . '"';
+			}
+		}
 		return $value;
 	}
 
+	// <tr> Schema Data is generated here
     public function tr_custom_attr( $data, $table_id, $row_idx, $table_data_row_idx ) {
 		$id = $table_id['id'];
-		$value = '$id="' . $id . '"$table_data_row_idx="' . $table_data_row_idx . '"';
+		$value = '';
+
+		// Check to see if we are in a header column
+		if ( 1 !== $table_data_row_idx ){
+			global $wpdb;
+			$schema_data = $wpdb->get_var("SELECT schema_data FROM {$wpdb->prefix}tablepress_schema_data WHERE table_id = $id");
+			$schema_data = unserialize( $schema_data );
+
+			// Check to make sure a itemtype value has been set
+			if ( !empty( $schema_data[0] ) ){
+				$schema_data_itemtype = $schema_data[0];
+				$value = 'itemscope itemtype="' . $schema_data_itemtype . '"';
+			}
+		}
 		return $value;
 	}
 
-} // class TablePress_Schema_Data_View
-
-
-
-/*
-tbody	id
-th		/id	/row
-tr		/id	/row
-td		id	/row	/column
-*/
+} // class TablePress_Render_Schema_Data
